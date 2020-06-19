46b2efa9189a2e78c023058741cf73a22b124261
jsonX <949463763@qq.com>
2020-06-19 19:44:41 +0800

    重新提交

---
+++ src/components/Login.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    登陆组件
+    <div class="tt">呜呜呜呜呜呜呜</div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="less" scoped>
+div {
+  font-size: 20px;
+  .tt {
+    background-color:red
+  }
+}
+</style>
