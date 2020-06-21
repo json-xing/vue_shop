0c6f80b27eceeddbf9b1d475e8f68aeae920b1a4
json-xing <949463763@qq.com>
2020-06-21 08:24:47 +0800

    提交测试

---
+++ src/components/Test.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    test 组件 Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio
+    consequuntur delectus fuga incidunt ex! Fugiat, deleniti ipsam quas ut ab
+    eaque autem maxime commodi nulla amet dolorem est, quam velit ipsum ex
+    voluptatibus? Autem ducimus dolorum voluptas cupiditate nihil beatae
+    corporis, repellat, hic voluptates saepe eligendi deserunt ex omnis iste.
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+<style lang=""></style>
