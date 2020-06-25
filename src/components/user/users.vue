44bed219942484fb71d566b2a9662bb5cd3b638a
json-xing <949463763@qq.com>
2020-06-25 10:07:12 +0800

    完成用户列表功能开发

--- src/components/user/users.vue
+++ src/components/user/users.vue
@@ -233,17 +233,22 @@ export default {
     // 删除用户  async  await ??????????
     async removeUserById(id) {
       // 弹框确认删除
-      await this.$confirm('此操作将永久删除该用户, 是否继续?', '提示', {
-        confirmButtonText: '确定',
-        cancelButtonText: '取消',
-        type: 'warning'
-      })
-        .then(() => {
-          return this.$message.success('删除成功')
-        })
-        .catch(() => {
-          return this.$message.info('已取消删除')
-        })
+      const res = await this.$confirm(
+        '此操作将永久删除该用户, 是否继续?',
+        '提示',
+        {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning'
+        }
+      ).catch(err => err)
+      if (res !== 'confirm') return this.$message.info('已取消删除')
+      const { data: result } = await this.$http.delete(`users/${id}`)
+      if (result.meta.status !== 200) {
+        return this.$message.error('删除失败请重试!')
+      }
+      this.$message.success('删除成功')
+      this.getUserList()
     },
     // 发起修改用户信息请求
     async editRequest(id) {
